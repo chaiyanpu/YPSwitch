89d99f1efe317e981e245b1be3b7ba53197ae185
chaiyanpu <chaiyanpu@126.com>
2016-12-09 10:27:10 +0800

    switch start
    switch start

---
+++ YPSwitch/YPSwitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YPSwitch
+//
+//  Created by chai on 2016/12/8.
+//  Copyright © 2016年 chaiyanpu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
